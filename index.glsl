172f6cd63496bd7cfc674784a70df5f2a8a35586
Mikola Lysenko <mikolalysenko@gmail.com>
2014-10-15 12:00:11 -0500

    factor out distribution into separate module

--- index.glsl
+++ index.glsl
@@ -1,17 +1,11 @@
+#pragma glslify: distribution = require(./distribution.glsl)
+
 float beckmannSpecular(
   vec3 lightDirection,
   vec3 viewDirection,
   vec3 surfaceNormal,
   float roughness) {
-
-  vec3 H = normalize(lightDirection + viewDirection);
-  float NdotH = max(dot(surfaceNormal, H), 0.0);
-  float cos2Alpha = NdotH * NdotH;
-  float tan2Alpha = (cos2Alpha - 1.0) / cos2Alpha;
-  float roughness2 = roughness * roughness;
-  float denom = 3.141592653589793 * roughness2 * cos2Alpha * cos2Alpha;
-
-  return exp(tan2Alpha / roughness2) / denom;
+  return distribution(dot(surfaceNormal, normalize(lightDirection + viewDirection)), roughness);
 }
 
 #pragma glslify: export(beckmannSpecular)
