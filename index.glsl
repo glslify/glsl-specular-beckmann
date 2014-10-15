eb573742fc4808560ec98268de246a7d0c7bc28b
Mikola Lysenko <mikolalysenko@gmail.com>
2014-10-15 10:49:14 -0500

    remove pi macro

--- index.glsl
+++ index.glsl
@@ -1,5 +1,3 @@
-#define PI 3.141592653589793
-
 float beckmannSpecular(
   vec3 lightDirection,
   vec3 viewDirection,
@@ -11,7 +9,7 @@ float beckmannSpecular(
   float cos2Alpha = NdotH * NdotH;
   float tan2Alpha = (cos2Alpha - 1.0) / cos2Alpha;
   float roughness2 = roughness * roughness;
-  float denom = PI * roughness2 * cos2Alpha * cos2Alpha;
+  float denom = 3.141592653589793 * roughness2 * cos2Alpha * cos2Alpha;
 
   return exp(tan2Alpha / roughness2) / denom;
 }
